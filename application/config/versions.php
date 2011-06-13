c2954b74efa1e2c84caac1dc795c42571deb45b1
zhanghedong <zhanghedong@gmail.com>
2011-06-13 10:56:50 +0000

    KO3 base

---
+++ application/config/versions.php
@@ -0,0 +1,13 @@
+<?php
+return array
+(
+    '1.0.0' => array(
+        'codename' => 'zhanghd',
+        'download' => 'files/lg.rar',
+        'documentation' => 'docs/1.0.0',
+        'released' => '06/05/2011',
+        'issues' => 'link/to/bug/tracker',
+    ),
+ 
+	
+);
